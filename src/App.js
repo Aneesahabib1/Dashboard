51a13b3ee8ef3278001330b473fb89a5fda7a812
Aneesahabib1 <aneesahabib145@gmail.com>
2023-05-10 18:53:14 +0500

    menu bars & charts

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { ColorModeContext, useMode} from "./scenes/theme";
+import { useState } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Navbar from "./scenes/global/Navbar";
+import Topbar from "./scenes/global/Topbar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import EmpMD from "./scenes/empMD/index";
+import Calendar from "./scenes/calendar/calendar";
+import Bar from "./scenes/bar";
+import Bar2 from "./scenes/bar2";
+import Pie from "./scenes/Pie";
+import Line from "./scenes/line";
+/*import TaskManagement from "./scenes/taskM";
+import Attendance from "./scenes/Attendance";
+import Departments from "./scenes/department";
+import Feedback from "./scenes/feedback";*/
+
+
+function App() {
+  const [theme, colorMode]= useMode();
+  const [isNavbar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Navbar isNavbar={Navbar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} /> 
+              
+              <Route path="/index" element={<EmpMD />} />
+             {/* <Route path="/TaskManagement" element={<TaskManagement />} />
+              <Route path="/Attendance" element={<Attendance />} />
+  <Route path="/Departments" element={<Departments />} />*/}
+                       <Route path="/calendar" element={<Calendar />} />
+                       <Route path="/bar" element={<Bar />} />
+                       <Route path="/Pie" element={<Pie />} />
+                       <Route path="/line" element={<Line />} />
+                       <Route path="/bar2" element={<Bar2 />} />
+{/*
+  <Route path="/Feedback" element={<Feedback />} />*/}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
